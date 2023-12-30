fbe54cf1a9702d3b3733c51ebbe8d9eb4193781e
Mykyta Onishchenko <on11shchenko@gmail.com>
2023-12-30 16:44:34 +0200

    setting database pg + typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.72.243',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
